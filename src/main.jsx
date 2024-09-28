8389c088c7520ab6c2fae2d9258d7628f224ed47
RafaelTecnicoDoLar <rjonasmombellifranco@gmail.com>
2024-09-28 15:41:02 -0300

    backup alteração header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    < Header/>
   </StrictMode>,
 )
