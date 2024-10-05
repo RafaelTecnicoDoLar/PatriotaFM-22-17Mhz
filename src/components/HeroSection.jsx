3b662b0678f981c26f5800d5d97088295f5f5286
RafaelTecnicoDoLar <rjonasmombellifranco@gmail.com>
2024-10-05 14:59:09 -0300

    finalizacao do heroselection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,40 @@
+import Listen from "../assets/albuns/listen-hellskitchen.jpg"
+import Sound from "../assets/audio/Listen.mp3"
+
 function HeroSection() {
     return (
-        <Selection>
-            <h1>
-                MEU TESTEZAO
+        <section class="sabadou" id="imgsabadou">
+            <div class="HeroSection">
+                <div class="player">
+                    <audio controls="controls">
+                        <source src={Sound} type="audio/mp3" />
+                        seu navegador não suporta HTML5
+                    </audio>
 
-            </h1>
+                    <h1 class="aovivo">
+                        ao vivo
+                    </h1>
 
-        </Selection>
+                </div>
+
+
+                <img class="imglisten" src={Listen} />
+
+                <div>
+
+                    <h2>
+                        Listen - Hellskitchen
+                    </h2>
+
+                    <h3 class="frase">
+                        Sabadou VOX com Hellskitchen
+                    </h3>
+
+                </div>
+
+            </div>
+
+        </section>
 
     );
 
