a55ab9d9444a6a1c2f1d510f59d7753088dc8477
RafaelTecnicoDoLar <rjonasmombellifranco@gmail.com>
2024-10-25 13:16:15 -0300

    continuação mais pedidos

--- src/components/BoxMusic.jsx
+++ src/components/BoxMusic.jsx
@@ -7,71 +7,119 @@ import CrackCocainemp3 from "../assets/audio/CrackCocaine.mp3"
 import PaperCut from "../assets/albuns/Papercut.jpeg"
 import PaperCutmp3 from "../assets/audio/PaperCut.mp3"
 
+import Eruption from "../assets/albuns/Eruption.jpeg"
+import Eruptionmp3 from "../assets/audio/Eruption.mp3"
+
+import KnockinOnHeavensDoor from "../assets/albuns/KnockingOnHeavensDoor.jpeg"
+import KnockinOnHeavensDoormp3 from "../assets/audio/knockinOnHeavesDoor.mp3"
+
 function BoxMusic() {
     return (
-        <section>
+        <div>
             <h1 class="Titulo">
                 As mais pedidas
             </h1>
 
-            <div class="card">
-                <img src={HolyWars} />
+            <section class="maispedidas">
 
-                <h2 class="autor">
-                    MEGADETH
-                </h2>
+                <div class="card">
+                    <img src={HolyWars} class="imgboxmusic"/>
 
-                <p class="subtitulo">
-                    Holy Wars The Punishment Due
-                </p>
+                    <h2 class="autor">
+                        MEGADETH
+                    </h2>
 
-                <audio controls="controls">
-                    <source src={HolyWarsmp3} type="audio/mp3" />
-                    seu navegador não suporta HTML5
+                    <p class="subtitulo">
+                        Holy Wars The Punishment Due
+                    </p>
 
-                </audio>
+                    <audio controls="controls">
+                        <source src={HolyWarsmp3} type="audio/mp3" />
+                        seu navegador não suporta HTML5
 
-            </div>
+                    </audio>
 
-            <div class="card">
-                <img src={CrackCocaine} />
+                </div>
 
-                <h2 class="autor">
-                    OZZY OSBOURNE
-                </h2>
+                <div class="card">
+                    <img src={CrackCocaine} class="imgboxmusic"/>
 
-                <p class="subtitulo">
-                Crack Cocaine
-                </p>
+                    <h2 class="autor">
+                        OZZY OSBOURNE
+                    </h2>
 
-                <audio controls="controls">
-                    <source src={CrackCocainemp3} type="audio/mp3" />
-                    seu navegador não suporta HTML5
+                    <p class="subtitulo">
+                        Crack Cocaine
+                    </p>
 
-                </audio>
+                    <audio controls="controls">
+                        <source src={CrackCocainemp3} type="audio/mp3" />
+                        seu navegador não suporta HTML5
 
-            </div>
+                    </audio>
 
-            <div class="card">
-                <img src={PaperCut} />
+                </div>
 
-                <h2 class="autor">
-                    LINKIN PARK
-                </h2>
+                <div class="card">
+                    <img src={PaperCut} class="imgboxmusic"/>
 
-                <p class="subtitulo">
-                PaperCut
-                </p>
+                    <h2 class="autor">
+                        LINKIN PARK
+                    </h2>
 
-                <audio controls="controls">
-                    <source src={PaperCutmp3} type="audio/mp3" />
-                    seu navegador não suporta HTML5
+                    <p class="subtitulo">
+                        PaperCut
+                    </p>
 
-                </audio>
+                    <audio controls="controls">
+                        <source src={PaperCutmp3} type="audio/mp3" />
+                        seu navegador não suporta HTML5
 
-            </div>
+                    </audio>
 
-        </section>
+                </div>
+
+                <div class="card">
+                    <img src={Eruption} class="imgboxmusic"/>
+
+                    <h2 class="autor">
+                        Van Halen
+                    </h2>
+
+                    <p class="subtitulo">
+                        Eruption 2015 Remaster
+                    </p>
+
+                    <audio controls="controls">
+                        <source src={Eruptionmp3} type="audio/mp3" />
+                        seu navegador não suporta HTML5
+
+                    </audio>
+
+                </div>
+
+                <div class="card">
+                    <img src={KnockinOnHeavensDoor} class="imgboxmusic"/>
+
+                    <h2 class="autor">
+                        Van Halen
+                    </h2>
+
+                    <p class="subtitulo">
+                        Eruption 2015 Remaster
+                    </p>
+
+                    <audio controls="controls">
+                        <source src={KnockinOnHeavensDoormp3} type="audio/mp3" />
+                        seu navegador não suporta HTML5
+
+                    </audio>
+
+                </div>
+
+            </section>
+
+        </div>
     );
 }
 
