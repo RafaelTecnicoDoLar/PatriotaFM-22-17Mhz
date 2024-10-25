6968727b8b062f42fc1a6d91c2f6df1a3ec14758
RafaelTecnicoDoLar <rjonasmombellifranco@gmail.com>
2024-10-25 10:03:33 -0300

    Anuncio finalizado

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,12 @@
+import Anuncio from "../assets/Anuncio.jpeg"
+
+function Announcement() {
+    return (
+        <div>
+            <a href="https://www.lojadomecanico.com.br/" target="_blank"> 
+            <img src={Anuncio} class="anuncio" /> </a>
+
+        </div>
+    );
+}
+export default Announcement;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import './index.css'
 
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
-
+import Announcement from './components/Announcement'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <HeroSection/>
+    <Announcement/>
   </StrictMode>
 )
