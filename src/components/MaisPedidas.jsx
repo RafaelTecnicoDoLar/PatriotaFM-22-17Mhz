7fca31ab49ddfd656eb78d4aa950abcb8dd7c989
RafaelTecnicoDoLar <rjonasmombellifranco@gmail.com>
2024-10-26 15:01:09 -0300

    carrossel mais pedidas em andamento

--- src/components/MaisPedidas.jsx
+++ src/components/MaisPedidas.jsx
@@ -15,19 +15,37 @@ import Eruptionmp3 from "../assets/audio/Eruption.mp3"
 import KnockinOnHeavensDoor from "../assets/albuns/KnockingOnHeavensDoor.jpeg"
 import KnockinOnHeavensDoormp3 from "../assets/audio/knockinOnHeavesDoor.mp3"
 
+import Territory from "../assets/albuns/Territory.jpg"
+import Territorymp3 from "../assets/audio/Territory.mp3"
+
+import TheLegacy from "../assets/albuns/TheLegacy.jpg"
+import TheLegacymp3 from "../assets/audio/TheLegacy.mp3"
+
+import WastedYears from "../assets/albuns/WastedYears.jpg"
+import WastedYearsmp3 from "../assets/audio/WastedYears.mp3"
+
+import Polly from "../assets/albuns/Polly.jpg"
+import Pollymp3 from "../assets/audio/Polly.mp3"
+
+
 function MaisPedidas() {
     return (
-        <div>
-            <h1 class="Titulo">
+        <div className="containerm-maispedidas">
+            <h1 className="Titulo">
                 As mais pedidas
             </h1>
 
-            <section class="maispedidas">
+            <section className="maispedidas">
                 <BoxMusic capa={HolyWars} nome="MEGADETH" musica="Holy Wars The Punishment Due" audio={HolyWarsmp3} />
                 <BoxMusic capa={CrackCocaine} nome="OZZY OSBOURNE" musica="Crack Cocaine" audio={CrackCocainemp3} />
                 <BoxMusic capa={PaperCut} nome="LINKIN PARK" musica="PaperCut" audio={PaperCutmp3} />
                 <BoxMusic capa={Eruption} nome="VAN HALEN" musica="Eruption 2015 Remaster" audio={Eruptionmp3} />
                 <BoxMusic capa={KnockinOnHeavensDoor} nome="GUNS N' ROSES " musica=" knockin on heaven's door" audio={KnockinOnHeavensDoormp3} />
+
+                <BoxMusic capa={Territory} nome="SEPULTURA" musica="Territory" audio={Territorymp3} />
+                <BoxMusic capa={WastedYears} nome="IRON MAIDEN" musica="Wasted Years - 2015 Remaster" audio={WastedYearsmp3} />
+                <BoxMusic capa={TheLegacy} nome="TESTAMENT" musica="The Legacy" audio={TheLegacymp3} />
+                <BoxMusic capa={Polly} nome="NIRVANA " musica="Polly" audio={Pollymp3} />
 
             </section>
 
